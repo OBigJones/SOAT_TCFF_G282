a078fabe5a36a93df95e40078e6e72f5b988a680
Donato Dutra <ddutra9@gmail.com>
2025-06-02 21:49:35 -0300

    docker compose esta subindo com swagger

--- API/Program.cs
+++ API/Program.cs
@@ -22,11 +22,8 @@ builder.Services.AddDependencies();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
